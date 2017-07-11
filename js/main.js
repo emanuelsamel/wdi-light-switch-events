c025e84a3ff694610678a65fec56c9c9ef6d77f5
emanuelsamel <mannys94@hotmail.com>
2017-07-11 18:12:27 -0400

    lights out

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
 
+document.addEventListener("DOMContentLoaded", function(){
 
+  // var state = 1
+var button = document.querySelector("button");
+
+  button.addEventListener("click", function(){
+  var body = document.querySelector("body");
+  var text = document.querySelector(".status");
+
+  if (button.getAttribute("class") === "switch on") {
+    body.setAttribute("class", "dark");
+    button.setAttribute("class","switch off");
+    text.innerHTML = "Hey who turned off the lights?"
+  } else {
+    body.setAttribute("class", "light");
+    button.setAttribute("class","switch on");
+    text.innerHTML = "It's so bright in here!"
+  };
+});
+
+
+
+});
